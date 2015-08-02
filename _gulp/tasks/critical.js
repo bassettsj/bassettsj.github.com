8e0aade1f9db89a60b6381ecd19e5e7366a71a0a
Steven Bassett <steven.j.bassett@gmail.com>
2015-08-02 15:29:46 -0700

    feat(gulp): add critical for css critical path

---
+++ _gulp/tasks/critical.js
@@ -0,0 +1,12 @@
+import gulp from 'gulp'
+import {stream as critical} from 'critical'
+
+gulp.task('critical', () => {
+  return gulp.src('_site/*.html')
+    .pipe(critical({
+        base: '_site/',
+        inline: true,
+        css: ['_site/assets/css/i.css']
+      }))
+      .pipe(gulp.dest('_site'))
+})
--- _gulp/tasks/publish.js
+++ _gulp/tasks/publish.js
@@ -1,7 +1,7 @@
 import gulp from 'gulp'
 import ghPages from 'gulp-gh-pages'
 
-gulp.task('publish', ['default'], () => {
+gulp.task('publish', ['default', 'critical'], () => {
   return gulp.src('_site/**/*')
     .pipe(ghPages({
       branch: 'master'
